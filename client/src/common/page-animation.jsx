8ebd59a5238d1d07a139f0b55b13757db9dc7d3d
Natnael Abnew <natnaelanbewpro@gmail.com>
2025-09-30 09:41:58 +0300

    feat: page entrance and exit animation

--- client/src/common/page-animation.jsx
+++ client/src/common/page-animation.jsx
@@ -1,1 +1,25 @@
-import { Animate, AnimatePresence, motion } from "framer-motion";
+import { animate, AnimatePresence, motion } from "motion/react-client";
+export default function AnimationWrapper({
+  children,
+  initial = { opacity: 0, scale: 0.5 },
+  animate = { opacity: 1, scale: 1 },
+  exit = { opacity: 0, scale: 0.9 },
+  transition = { duration: 0.8, delay: 0.5, ease: [0, 0.71, 0.2, 1.01] },
+  keyValue,
+  className,
+}) {
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        initial={initial}
+        animate={animate}
+        exit={exit}
+        transition={transition}
+        key={keyValue}
+        className={className}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  );
+}
