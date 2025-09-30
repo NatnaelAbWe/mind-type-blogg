f343c34e4f58e40772b49c0f255b9d505b6139b2
Natnael Abnew <natnaelanbewpro@gmail.com>
2025-09-30 19:52:58 +0300

    feat: add a route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import UserAuthForm from "./pages/userAuthForm";
 
 function App() {
   return (
-    <>
-      <NavBar />
-    </>
+    <Routes>
+      <Route path="/" element={<NavBar />}>
+        <Route path="/signin" element={<UserAuthForm type="signin" />} />
+        <Route path="/signup" element={<UserAuthForm type="signup" />} />
+      </Route>
+    </Routes>
   );
 }
-
 export default App;
