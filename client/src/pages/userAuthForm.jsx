12ac114caf3872ca8c55329cc70b26f013f2a18a
Natnael Abnew <natnaelanbewpro@gmail.com>
2025-10-02 17:18:40 +0300

    frontend resources and files

--- client/src/pages/userAuthForm.jsx
+++ client/src/pages/userAuthForm.jsx
@@ -3,11 +3,63 @@ import { useRef } from "react";
 import { Link } from "react-router-dom";
 import InputBox from "../components/Input";
 import GoogleLogo from "../imgs/google.png";
+import { Toaster, toast } from "react-hot-toast";
+
 export default function UserAuthForm({ type }) {
   const AuthForm = useRef();
+  function userAuthThroughServer(serverRoute, formData) {
+    fetch(`http://localhost:3000/${type}`, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log("server response:", data))
+      .catch((err) => console.log("error:", err));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let serverRoute = type === "signin" ? "/signin" : "/signup";
+
+    let form = new FormData(AuthForm.current);
+    let formData = {};
+
+    for (let [key, value] of form.entries()) {
+      formData[key] = value.trim();
+    }
+
+    // form validation
+    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/;
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+    let { fullname, email, password } = formData;
+
+    if (type === "signup" && (!fullname || fullname.length < 3)) {
+      return toast.error("Full Name must be at least 3 characters long.");
+    }
+
+    if (!email) {
+      return toast.error("Enter an Email.");
+    } else if (!emailRegex.test(email)) {
+      return toast("Invalid Email format.");
+    }
+
+    if (!passwordRegex.test(password)) {
+      return toast.error(
+        "Password must be 6–20 characters, include a number, lowercase and uppercase letter."
+      );
+    }
+
+    //  Passed validation
+    console.log("Form data ready to send:", formData);
+    userAuthThroughServer(serverRoute, formData);
+  }
+
   return (
     <AnimationWrapper>
       <section className="h-cover flex item-center justify-center">
+        <Toaster />
         <form ref={AuthForm} className="w-[80%] max-w-[400px]">
           <h1 className="text-4xl font-glasio capitalize text-center mb-4 mt-4">
             {type === "signin" ? "Welcome Back" : "Join Us Today"}
@@ -41,7 +93,7 @@ export default function UserAuthForm({ type }) {
           <button
             className="btn-dark center mt-14"
             type="submit"
-            // onClick={handleSubmit}
+            onClick={handleSubmit}
           >
             {type === "signin" ? "Sign In" : "Sign up"}
           </button>
